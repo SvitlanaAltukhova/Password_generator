e60a99ac602ea2ed21203638603d2b8b3366e5a2
SvitlanaAltukhova <86447937+SvitlanaAltukhova@users.noreply.github.com>
2021-12-21 18:39:26 +0200

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -5,7 +5,7 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-print("!!!Remember if you won't answer for the last question (or answer something beside 's' or 'r') program will automaticaly in standart order!!!")
+print("!!!Remember if you won't answer for the last question (or answer something beside 's' or 'r') program will run automaticaly in standart order!!!")
 
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
@@ -30,4 +30,4 @@ if order == "r":
       random.shuffle(password_elements)
       
 password = "".join(password_elements)
-print(f"Your password: {password}")
+print(f"Your password is: {password}")
